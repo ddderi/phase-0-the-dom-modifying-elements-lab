54814e8ce08a8f95e343c7ad1583d4530f75736b
ddderi <maxime830@live.fr>
2022-01-17 03:50:46 +0100

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const Main = document.querySelector('main')
+Main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+
+
+newHeader.innerHTML = `<h1>Maxime is the champion</h1>`
