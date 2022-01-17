05c1992a279887e999ac3345743a858772cd10ca
ddderi <maxime830@live.fr>
2022-01-17 03:45:40 +0100

    commit tree 9b0504221be7900a68504d520c4bfca0be548d26

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const Main = document.querySelector('main')
+Main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+//const headerr = document.getElementsByTagName('h1').innerHTML = `<h1 id=\'victory\'>Maxime is the champion</h1>`
+
+newHeader.innerHTML = `<h1>Maxime is the champion</h1>`
